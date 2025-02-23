581807ed55d97641f02c0346f65d90e36729d59d
murielle-beaulieu <murielle.beaulieu19@gmail.com>
2025-02-23 21:24:55 +1100

    x

---
+++ src/main/java/io/nology/todo_api/todo/TodoController.java
@@ -0,0 +1,42 @@
+package io.nology.todo_api.todo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+  private TodoService todoService;
+
+  TodoController(TodoService todoService) {
+    this.todoService = todoService;
+  }
+
+
+  // READ ALL
+  @GetMapping
+  public String getAllTodos() {
+      return "all todos";
+  }
+
+  // READ ONE
+  @GetMapping("/{id}")
+  public String getMethodName(@RequestParam String param) {
+      return new String();
+  }
+
+
+  // CREATE
+
+  // UPDATE
+
+  // DELETE
+
+
+
+
+}
